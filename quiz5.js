0038a2d6b462d738850f38a495c9073c6958353f
4212ganteng <azizmuslim051@gmail.com>
2023-12-20 09:50:57 +0700

    quize 5

--- quiz5.js
+++ quiz5.js
@@ -6,19 +6,32 @@
 //  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
 //   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
 
-let word1 = "JavaScript ";
-let second = "is ";
-let third = "awesome ";
-let fourth = "and ";
-let fifth = "I ";
-let sixth = "love ";
+let word1 = "JavaScript";
+let second = "is";
+let third = "awesome";
+let fourth = "and";
+let fifth = "I";
+let sixth = "love";
 let seventh = "it!";
 
 //code here
-const sentence = word1 + second + third + fourth + fifth + sixth + seventh;
+const sentence =
+  word1 +
+  " " +
+  second +
+  " " +
+  third +
+  " " +
+  fourth +
+  " " +
+  fifth +
+  " " +
+  sixth +
+  " " +
+  seventh;
 
 // ---------------------------------------
-console.log("sentence:  " + sentence);
+console.log(sentence);
 // ---------------------------------------
 
 // 2. Index Accessing - 1 by 1
